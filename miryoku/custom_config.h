3e0824900fe07d565f3351ad0116ba8c92a55562
B. Koop <5205713+nivoset@users.noreply.github.com>
2025-08-07 09:40:26 -0400

    flip

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,12 +3,12 @@
 
 #define MIRYOKU_LAYERS_FLIP
 
-#define MIRYOKU_LAYERMAPPING_NUM( ...) \ 
-  {/* Left hand (Nav cluster) */ \
-   &kp HOME, &kp LEFT, &kp DOWN, &kp UP, &kp RIGHT, &kp END,    \
-   /* Right hand (Numpad cluster) */ \
-   &kp N7,   &kp N8,   &kp N9,   &kp KP_PLUS, &kp BSPC,         \
-   &kp N4,   &kp N5,   &kp N6,   &kp KP_MINUS, &kp ENTER,       \
-   &kp N1,   &kp N2,   &kp N3,   &kp KP_ASTERISK, &kp DELETE,   \
-   /* ... etc ... */ \
-  }
+// #define MIRYOKU_LAYERMAPPING_NUM( ...) \ 
+//   {/* Left hand (Nav cluster) */ \
+//    &kp HOME, &kp LEFT, &kp DOWN, &kp UP, &kp RIGHT, &kp END,    \
+//    /* Right hand (Numpad cluster) */ \
+//    &kp N7,   &kp N8,   &kp N9,   &kp KP_PLUS, &kp BSPC,         \
+//    &kp N4,   &kp N5,   &kp N6,   &kp KP_MINUS, &kp ENTER,       \
+//    &kp N1,   &kp N2,   &kp N3,   &kp KP_ASTERISK, &kp DELETE,   \
+//    /* ... etc ... */ \
+//   }
