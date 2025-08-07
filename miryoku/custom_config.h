5a1e73bab5a877360cf0ebd02cc5ba61c78788de
B. Koop <5205713+nivoset@users.noreply.github.com>
2025-08-07 09:13:12 -0400

    test

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,14 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_LAYERS_FLIP
+
+#define MIRYOKU_LAYERMAPPING_NUM( ...) \ 
+  {/* Left hand (Nav cluster) */ \
+   &kp HOME, &kp LEFT, &kp DOWN, &kp UP, &kp RIGHT, &kp END,    \
+   /* Right hand (Numpad cluster) */ \
+   &kp N7,   &kp N8,   &kp N9,   &kp KP_PLUS, &kp BSPC,         \
+   &kp N4,   &kp N5,   &kp N6,   &kp KP_MINUS, &kp ENTER,       \
+   &kp N1,   &kp N2,   &kp N3,   &kp KP_ASTERISK, &kp DELETE,   \
+   /* ... etc ... */ \
+  }
